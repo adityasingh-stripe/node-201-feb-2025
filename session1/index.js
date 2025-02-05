374a8fe9704e4aba2d6448ab00608f08151a86f3
Aditya Singh <106972852+aditya-docs@users.noreply.github.com>
2025-02-05 15:43:44 +0000

    session1 - http server

---
+++ session1/index.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const PORT = 8082;
+
+// const server = http.createServer((req, res) => {
+//   //   res.write("Hello from server");
+//   //   res.write("\nBye from server");
+//   //   res.end("\nFinished!");
+//   const serverInfo = {
+//     serverName: "Crio Server",
+//     version: "1.0.0",
+//     currentDate: new Date().toDateString(),
+//     currentTime: new Date().toTimeString(),
+//   };
+//   res.writeHead(200, { "content-type": "application/json" });
+//   res.end(JSON.stringify(serverInfo));
+//   //   res.end();
+// });
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1>Hello from Server</h1>");
+  } else if (req.url === "/info" && req.method === "GET") {
+    const serverInfo = {
+      serverName: "Crio Server",
+      version: "1.0.0",
+      currentDate: new Date().toDateString(),
+      currentTime: new Date().toTimeString(),
+    };
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify(serverInfo));
+  } else {
+    res.writeHead(404, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Route does not exist!",
+      })
+    );
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`);
+});
