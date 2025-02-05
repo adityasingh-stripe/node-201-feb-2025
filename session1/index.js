823a46d53bd58a7e459628f8bbd01e6a7eeed9e3
Aditya Singh <106972852+aditya-docs@users.noreply.github.com>
2025-02-05 16:05:04 +0000

    session1 - currencies code demo

--- session1/index.js
+++ session1/index.js
@@ -1,6 +1,6 @@
 const http = require("http");
+const currenciesJson = require("./currencies.json");
 const PORT = 8082;
-
 // const server = http.createServer((req, res) => {
 //   //   res.write("Hello from server");
 //   //   res.write("\nBye from server");
@@ -16,26 +16,49 @@ const PORT = 8082;
 //   //   res.end();
 // });
 
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/" && req.method === "GET") {
+//     res.writeHead(200, { "content-type": "text/html" });
+//     res.end("<h1>Hello from Server</h1>");
+//   } else if (req.url === "/info" && req.method === "GET") {
+//     const serverInfo = {
+//       serverName: "Crio Server",
+//       version: "1.0.0",
+//       currentDate: new Date().toDateString(),
+//       currentTime: new Date().toTimeString(),
+//     };
+//     res.writeHead(200, { "content-type": "application/json" });
+//     res.end(JSON.stringify(serverInfo));
+//   } else {
+//     res.writeHead(404, { "content-type": "application/json" });
+//     res.end(
+//       JSON.stringify({
+//         message: "Route does not exist!",
+//       })
+//     );
+//   }
+// });
+
 const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end("<h1>Hello from Server</h1>");
-  } else if (req.url === "/info" && req.method === "GET") {
-    const serverInfo = {
-      serverName: "Crio Server",
-      version: "1.0.0",
-      currentDate: new Date().toDateString(),
-      currentTime: new Date().toTimeString(),
-    };
-    res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify(serverInfo));
-  } else {
-    res.writeHead(404, { "content-type": "application/json" });
-    res.end(
-      JSON.stringify({
-        message: "Route does not exist!",
-      })
-    );
+  let symbol;
+  const pathArr = req.url.split("/");
+  if (pathArr[1] === "currencies" && pathArr.length === 3) symbol = pathArr[2];
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { "content-type": "text/html" });
+      res.end("<h1>Currency database</h1>");
+      break;
+    case "/currencies":
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify(currenciesJson.data));
+      break;
+    default:
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Route does not exist!",
+        })
+      );
   }
 });
 
