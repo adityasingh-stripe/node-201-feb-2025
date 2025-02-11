48c7e5d03f26429807b9063edcda9783b23a55a9
Aditya Singh <106972852+aditya-docs@users.noreply.github.com>
2025-02-11 16:00:17 +0000

    Session3 - joi

--- session3/controllers/users.controller.js
+++ session3/controllers/users.controller.js
@@ -1,4 +1,6 @@
 const { data } = require("../users.json");
+const { validGenders } = require("../config.js");
+const userSearchSchema = require("../validations/userSearch.validation.js");
 
 const getUsers = (req, res) => {
   res.send(data);
@@ -16,6 +18,25 @@ const getUserById = (req, res) => {
 
 const searchUsers = (req, res) => {
   const { gender, age } = req.query;
+
+  const { error } = userSearchSchema.validate({ gender, age });
+  if (error)
+    return res.status(400).send({ message: error.details[0].message, error });
+
+  // if (gender && !validGenders.includes(gender))
+  //   return res
+  //     .status(400)
+  //     .send({ message: `Gender must be one of: ${validGenders.join(", ")}` });
+  // if (
+  //   (age && isNaN(age)) ||
+  //   !Number.isInteger(Number(age)) ||
+  //   parseInt(age) <= 0 ||
+  //   parseInt(age) >= 100
+  // )
+  //   return res
+  //     .status(400)
+  //     .send({ message: `Age must be an integer between 1 to 99` });
+
   if (gender && age)
     return res.send(
       data.filter(
@@ -24,9 +45,9 @@ const searchUsers = (req, res) => {
     );
   if (gender) return res.send(data.filter((user) => user.gender === gender));
   if (age) return res.send(data.filter((user) => String(user.dob.age) === age));
-  res
-    .status(400)
-    .send({ message: `At least one of 'gender' or 'age' must be sent.` });
+  // res
+  //   .status(400)
+  //   .send({ message: `At least one of 'gender' or 'age' must be sent.` });
 };
 
 module.exports = { getUsers, getUserById, searchUsers };
---
+++ session3/validations/userSearch.validation.js
@@ -0,0 +1,9 @@
+const Joi = require("joi");
+const { validGenders } = require("../config.js");
+
+const userSearchSchema = Joi.object({
+  gender: Joi.string().valid(...validGenders),
+  age: Joi.number().integer().min(1).max(99),
+}).or("gender", "age");
+
+module.exports = userSearchSchema;
